b00d8f98ac1cc2e9264be3a58717067e23ac5f15
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-07-07 21:46:33 +0330

    fix : gauge meter bugs fixed

--- src/components/base/section-container.tsx
+++ src/components/base/section-container.tsx
@@ -9,7 +9,7 @@ export default function SectionContainer({
 }) {
 	return (
 		<section
-			className={`${extraClass} text-white p-3 rounded-3xl border border-gray-600 mx-auto shadow-2xl">`}
+			className={`${extraClass} text-white p-3 rounded-xl border border-gray-600 mx-auto shadow-2xl">`}
 		>
 			{children}
 		</section>
--- src/features/shekaf-sekke/components/shekaf-sekke.tsx
+++ src/features/shekaf-sekke/components/shekaf-sekke.tsx
@@ -2,17 +2,21 @@ import CustomH2 from "@/components/base/custom-h2";
 import SectionContainer from "@/components/base/section-container";
 import GoldGauge from "@/features/shekaf-sekke/components/gold-gauge";
 
+const shekafData = { abshodeh: 50, neutral: 0, robSekke: 150 };
+
 export default function ShekafSekke() {
 	return (
 		<SectionContainer extraClass="w-full">
 			<div className="flex flex-row justify-between items-center h-full">
 				<div className="w-50">
-					<GoldGauge abShodehScore={2} khonsaScore={5} robeSekkehScore={6} />
+					<GoldGauge abshodeh={50} neutral={0} robSekeh={150} />
 				</div>
 				<div className="h-full flex flex-col justify-between items-end gap-10">
 					<CustomH2 text="شکاف ربع سکه" />
-					<div className="text-[8px] text-center p-2 rounded-[7px] border border-[#E6B745]/50 bg-radial from-[#E6B745]/20 to-transparent text-shadow-2xs text-shadow-[#E6B745]/10 font-normal">
-						خرید طلا به صرفه تر است
+					<div className="text-[8px] text-center p-2 rounded-[7px] border border-[#E6B745]/50 bg-radial from-[#E6B745]/25 to-transparent text-shadow-2xs text-shadow-[#E6B745]/10 font-normal">
+						{shekafData.robSekke > shekafData.abshodeh
+							? "خرید ربع سکه به صرفه تر است"
+							: "خرید طلا به صرفه تر است"}
 					</div>
 				</div>
 			</div>
